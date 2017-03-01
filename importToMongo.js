67dd9b16d08d7202cf0cfe0a29f5e2a61c457df4
unknown <wujiang@SLWY.COM>
2017-03-01 22:51:21 +0800

    ’修改导入数据库脚本‘

--- importToMongo.js
+++ importToMongo.js
@@ -1,18 +1,31 @@
 const child_process = require('child_process');
 const path = require('path');
 const fs = require('fs');
+const async = require('async');
 
 const month = '201702';
-fs.readdirSync(path.join('data', month)).forEach((item, index) => {
+const files = fs.readdirSync(path.join('data', month))
+
+async.map(files, (item, callback) => {
     const filePath = path.join('data', month, item);
     const cmd = `mongoimport -d football -c match${month} --file ${filePath} --jsonArray`
 
-    child_process.exec(cmd, (err, stdout, stderr) => {
-        err && console.log(err.message);
+    child_process.execSync(cmd, (err, stdout, stderr) => {
+        const flat = true;
+        if (err) {
+            console.log(err.message)
+            flag = false;
+        }
         console.log('-----------------------------------------------')
         console.log(`File ${filePath} has been imported to MongoDB.`)
         console.log('-----------------------------------------------')
         stdout && console.log(stdout);
         stderr && console.log(stderr);
+        callback(null, flag);
     })
+}, (err, res) => {
+    err && console.log(err);
+    if (res) {
+        console.log('Import finished')
+    }
 })
